ce18244f4f4994a816d5d61690ef70e93b0450d8
Matheus Yamamoto Dias <matheusyamas@gmail.com>
2025-09-05 16:31:14 -0300

    v1

---
+++ src/listasencadeada/Fila.java
@@ -0,0 +1,65 @@
+package listasencadeada;
+
+import java.util.Scanner;
+
+public class Fila {
+    private Node Fila;
+    public Fila(){
+        this.Fila = null;
+    }
+// Rever o insere
+    public void insere (int informacao){
+        Node no = new Node();
+        no.setInformacao(informacao);
+        if(vazia()){
+            Fila=no;
+        }
+        else{
+            Node atual =Fila;
+            while(atual.getProximo()!=null){
+                atual = atual.getProximo();
+            }
+            atual.setProximo(no);
+            System.out.println("Nó atual no endereço" + atual);
+        }
+    }
+
+    public Node remove(){
+        if(vazia()){
+            return null;
+        }
+        Node no = Fila;
+        Fila = Fila.getProximo();
+        return no;
+    }
+
+    public void imprime(){
+        Node atual = Fila;
+        while (atual != null) {
+            System.out.print(atual.getInformacao()+ " -> ");
+            atual= atual.getProximo();
+        }
+        System.out.println("Null");
+    }
+
+    public boolean vazia(){
+        return Fila == null;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Fila fila = new Fila();
+        int [] itens = {1, 5, 3,9, 0};
+        System.out.println("Digite os itens da lista (digite '0' para parar):");
+        for (int item : itens){
+            if (item == 0){
+                break;
+            }
+            fila.insere(item);
+        }
+        System.out.println("Fila com Lista Encadeada:");
+        fila.imprime();
+        fila.remove();
+        fila.imprime();
+    }
+}
---
+++ src/listasencadeada/Pilha.java
@@ -0,0 +1,65 @@
+package listasencadeada;
+
+import java.util.Scanner;
+
+public class Pilha {
+    private Node Pilha;
+    public Pilha(){
+        this.Pilha = null;
+    }
+
+    public void insere(int informacao){
+        Node no = new Node();
+        no.setInformacao(informacao);
+
+        if(vazia()){
+            Pilha=no;
+        }
+        else{
+            no.setProximo(Pilha);
+            Pilha = no;
+
+            System.out.println("Nó atual no endereço" + no);
+        }
+    }
+
+
+    //Imprimindo elementos
+    public void imprime(){
+        Node atual = Pilha;
+        while (atual != null) {
+            System.out.print(atual.getInformacao()+ " -> ");
+            atual= atual.getProximo();
+        }
+        System.out.println("Null");
+    }
+
+    public boolean vazia(){
+        return Pilha == null;
+    }
+
+    public Node remove(){
+        if(vazia()){
+            return null;
+        }
+        Node no = Pilha;
+        Pilha = Pilha.getProximo();
+        return no;
+    }
+
+    public static void main(String[] args) {
+        Pilha pilha = new Pilha();
+        int [] itens = {1, 5, 3,9, 0};
+        System.out.println("Digite os itens da lista (digite '0' para parar):");
+        for (int item : itens){
+            if (item == 0){
+                break;
+            }
+            pilha.insere(item);
+        }
+        System.out.println("Pilha com lista Encadeada:");
+        pilha.imprime();
+        pilha.remove();
+        pilha.imprime();
+    }
+}
